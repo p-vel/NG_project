87a9ebec5a78a1223c57adfca5f0c66ce459e5b0
Pierre Veldeman <veldemanpierre@gmail.com>
2020-11-24 10:14:43 +0100

    Adding JackieChanCentered

--- src/main.c
+++ src/main.c
@@ -26,6 +26,10 @@ int main()
 	// random_points(coord, nPoints);
 	//circle_points(coord, nPoints);
 	coord = scanFile("../JackieChan.txt");
+	for (int i = 0; i < nPoints; i++) {
+		coord[i][0] = coord[i][0] / 500. - 1.;
+		coord[i][1] = coord[i][1] / 500. - 1;
+	}
 
 	clock_t t0 = clock();
   graham_scan(coord, nPoints, &my_hull_size, my_hull);
@@ -45,13 +49,14 @@ int main()
 #if ANIMATION_ON
 	bov_points_t *coordDraw = bov_points_new(coord, nPoints, GL_STATIC_DRAW);
 	bov_points_t *hullDraw = bov_points_new(my_hull, my_hull_size, GL_STATIC_DRAW);
+	bov_points_set_width(coordDraw, .002);
 	bov_points_set_color(coordDraw, (GLfloat[4]) {0.0, 0.0, 0.0, 1.0});
 	bov_points_set_color(hullDraw, (GLfloat[4]) {1.0, 0.0, 0.0, 1.0});
 	bov_points_set_outline_color(coordDraw, (GLfloat[4]) {0.3, 0.12, 0.0, 0.25});
 
 	while(!bov_window_should_close(window)){
 		bov_points_draw(window, coordDraw, 0, nPoints);
-		bov_points_draw(window, hullDraw, 0, my_hull_size);
+		//bov_points_draw(window, hullDraw, 0, my_hull_size);
 		//bov_fast_line_loop_draw(window, hullDraw, 0, my_hull_size);
 		bov_window_update(window);
 	}
